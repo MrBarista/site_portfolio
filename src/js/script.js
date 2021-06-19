b7c91486583e7d07a8bbde5c256f3ca44ef43de6
Boris <borisnevedrov@yandex.ua>
2021-06-19 11:23:29 +0300

    create section me, change font, update js file

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,16 @@
 const hamburger = document.querySelector('.hamburger'),
 	  menu = document.querySelector('.menu'),
 	  close = document.querySelector('.menu__close');
+	  overlayclose = document.querySelector('.menu__overlay');
 	
 hamburger.addEventListener('click',() => {
 	menu.classList.add('active');
 })
 
 close.addEventListener('click',() =>{
+	menu.classList.remove('active');
+})
+
+overlayclose.addEventListener('click',() =>{
 	menu.classList.remove('active');
 })
