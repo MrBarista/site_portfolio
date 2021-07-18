cb8d7e4415366d6027ea8d8ed2c29fc914e51ad5
Boris <borisnevedrov@yandex.ua>
2021-07-18 15:10:06 +0300

    arrow-up and validate_js

--- src/js/script.js
+++ src/js/script.js
@@ -14,3 +14,36 @@ close.addEventListener('click',() =>{
 overlayclose.addEventListener('click',() =>{
 	menu.classList.remove('active');
 })
+
+$(window).scroll(function(){
+	if ($(this).scrollTop() > 700 ){
+		$('.pageup').fadeIn();
+	} else {
+		$('.pageup').fadeOut();
+	}
+});
+
+/* $('.contacts__form').validate({
+	rules: {
+		name: {
+			required: true,
+			minlength: 2
+		  },
+		email: {
+			required: true,
+			email: true
+		},
+		checkbox: "required"
+	},
+	messages:{
+		name: {
+			required: "Пожалуйста введите своё имя",
+			minlength: jQuery.validator.format("Требуется минмимум {0} символа")
+		  },
+		email: {
+			required: "Пожалуйста введите почтовый адрес",
+			email: "Неправильно введён адрес почты"
+		},
+		checkbox: "Сначала вы должны согласиться с нашей политикой."
+	}
+}); */
